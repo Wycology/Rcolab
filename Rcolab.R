d296192727be1d8266d7bf896568d0a118ae5a09
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-06-08 11:40:48 +0300

    Double %%

--- Rcolab.R
+++ Rcolab.R
@@ -3,3 +3,8 @@
 
 # %load_ext rpy2.ipython
 
+# Once that is loaded successfully then you can goa ahead and load libraries like:
+
+# %R library(datasets) This is going to work on one line
+
+# In case you want multiple lines to be executed then use double % like:
