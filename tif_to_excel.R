7d36cba895e0921328d438c9a62616193f216046
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-06-15 17:16:06 +0300

    Corrected

--- tif_to_excel.R
+++ tif_to_excel.R
@@ -2,8 +2,8 @@
 # Task: Extracting raster values and saving them in Excel file as .xlsx   #
 # Author: Wyclife Agumba Oluoch                                           #
 # Contact: wyclifeoluoch@gmail.com                                        #
-# Date: Created on 15th June 2021                                         #
-# Last modified:                                                          #
+# Date: Created on 13th June 2021                                         #
+# Last modified:   15th June 2021                                         #
 ###########################################################################
 
 library(raster)     # For creating the raster objects and stacking them 
