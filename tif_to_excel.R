057b753586f27b3e84674955c10f1d92ba0aa05f
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-06-15 17:15:32 +0300

    June the 15th

--- tif_to_excel.R
+++ tif_to_excel.R
@@ -2,7 +2,7 @@
 # Task: Extracting raster values and saving them in Excel file as .xlsx   #
 # Author: Wyclife Agumba Oluoch                                           #
 # Contact: wyclifeoluoch@gmail.com                                        #
-# Date: Created on 1th June 2021                                         #
+# Date: Created on 15th June 2021                                         #
 # Last modified:                                                          #
 ###########################################################################
 
