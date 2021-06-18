378f5967ef344b6d5007d14d32e34fb85215f207
Wyclife Agumba Oluoch <wyclifeoluoch@gmail.com>
2021-06-18 18:29:12 +0300

    raster files creation

--- tif_to_excel.R
+++ tif_to_excel.R
@@ -9,7 +9,7 @@
 library(raster)   # For creating the raster objects and stacking them 
 library(xlsx)     # For handling the workbook and excel sheets
 
-# Create three raster files and stack them together
+# Create three raster  and stack them together
 
 r1 <- raster(nrows = 20, ncols = 20) # Creating a simple raster
 r1[] <- rnorm(n = ncell(r1)) # Filling it with random numbers
